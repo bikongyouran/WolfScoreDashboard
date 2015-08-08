5fa5e2f27dd3d817cafaac2d1cfcc67f1ce4a524
haifwu <haifwu@ebay.com>
2015-08-08 21:42:56 +0800

    initiald

--- pypl/views.py
+++ pypl/views.py
@@ -2,26 +2,20 @@
 from __future__ import unicode_literals
 
 from django.http.response import HttpResponse, HttpResponseBadRequest
-from django.views.decorators.csrf import csrf_exempt
 import hashlib
 
 from wechat_sdk import WechatBasic
 from wechat_sdk.exceptions import ParseError
 from wechat_sdk.messages import TextMessage
 
-WECHAT_TOKEN = 'recruit'
-AppID = 'wxffda7df5ae5ab2f9'
-AppSecret = '915878d1c58f69dff9139e3556789201'
-
 # 实例化 WechatBasic
 wechat_instance = WechatBasic(
-    token=WECHAT_TOKEN,
-    appid=AppID,
-    appsecret=AppSecret
+    token = "recruit",
+    appid = 'wxffda7df5ae5ab2f9',
+    appsecret = '915878d1c58f69dff9139e3556789201'
 )
 
 # Create your views here.
-@csrf_exempt
 def index(request):
     if request.method == 'GET':
         # 检验合法性
@@ -30,12 +24,10 @@ def index(request):
         timestamp = request.GET.get('timestamp')
         nonce = request.GET.get('nonce')
 
-        if not wechat_instance.check_signature(
-                signature=signature, timestamp=timestamp, nonce=nonce):
+        if not wechat_instance.check_signature(signature=signature, timestamp=timestamp, nonce=nonce):
             return HttpResponseBadRequest('Verify Failed')
 
-        return HttpResponse(
-            request.GET.get('echostr', ''), content_type="text/plain")
+        return HttpResponse(request.GET.get('echostr', ''), content_type="text/plain")
 
 
     # 解析本次请求的 XML 数据
