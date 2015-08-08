fc3914f8dcaad922865ce9db848164da44f93d96
haifwu <haifwu@ebay.com>
2015-08-08 12:39:11 +0800

    added pypl test

---
+++ pypl/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PyplConfig(AppConfig):
+    name = 'pypl'
---
+++ pypl/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+import hashlib
+
+
+# Create your views here.
+def au(request):
+    if request.method == 'GET':
+        signature = request.GET['signature']
+        timestamp = request.GET['timestamp']
+        nonce = request.GET['nonce']
+        echostr = request.GET['echostr']
+
+        token='recruit'
+        list = [token, timestamp, nonce]
+        list.sort()
+        sha1 = hashlib.sha1
+        map(sha1.update, list)
+        hashcode = sha1.hexdigest()
+
+        if hashcode == signature:
+            return HttpResponse(echostr)
