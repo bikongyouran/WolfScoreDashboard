93a9ec5d5ffab2b099396f4abb3f1add994f47b4
haifwu <haifwu@ebay.com>
2015-08-08 13:00:52 +0800

    adjust

--- pypl/views.py
+++ pypl/views.py
@@ -1,21 +1,46 @@
-from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from xml.etree import ElementTree as etree
+from django.utils.encoding import smart_str
 import hashlib
 
 
 # Create your views here.
 def au(request):
-    signature = request.GET['signature']
-    timestamp = request.GET['timestamp']
-    nonce = request.GET['nonce']
-    echostr = request.GET['echostr']
+    if request.method == 'GET':
+        response = HttpResponse(checkSignature(request))
+        return response
+    else:
+        xmlstr = smart_str(request.body)
+        xml = etree.fromstring(xmlstr)
 
-    token='recruit'
-    list = [token, timestamp, nonce]
-    list.sort()
-    sha1 = hashlib.sha1
-    map(sha1.update, list)
-    hashcode = sha1.hexdigest()
+        ToUserName = xml.find('ToUserName').text
+        FromUserName = xml.find('FromUserName').text
+        CreateTime = xml.find('CreateTime').text
+        MsgType = xml.find('MsgType').text
+        Content = xml.find('Content').text
+        MsgId = xml.find('MsgId').text
+        reply_xml = """<xml>
+       <ToUserName><![CDATA[%s]]></ToUserName>
+       <FromUserName><![CDATA[%s]]></FromUserName>
+       <CreateTime>%s</CreateTime>
+       <MsgType><![CDATA[text]]></MsgType>
+       <Content><![CDATA[%s]]></Content>
+       </xml>"""%(FromUserName,ToUserName,CreateTime,Content + "  Hello world, this is test message")
+        return HttpResponse(reply_xml)
 
-    if hashcode == signature:
-        return HttpResponse(echostr)
+
+def checkSignature(request):
+    signature = request.GET.get('signature', None)
+    timestamp = request.GET.get('timestamp', None)
+    nonce = request.GET.get('nonce', None)
+    echostr = request.GET.get('echostr', None)
+    token = "recruit"
+
+    tmplist = [token, timestamp, nonce]
+    tmplist.sort()
+    tmpstr = "%s%s%s" % tuple(tmplist)
+    tmpstr = hashlib.sha1(tmpstr).hexdigest()
+    if tmpstr == signature:
+        return echostr
+    else:
+        return None
